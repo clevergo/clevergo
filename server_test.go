b459ef89fd5a2ff38487d598bd30d86662a4857a
HeadwindFly <HeadwindFly@163.com>
2016-11-14 19:56:04 +0800

    Improved Server's tests

--- server_test.go
+++ server_test.go
@@ -6,6 +6,9 @@ package gem
 
 import (
 	"testing"
+
+	"github.com/go-gem/log"
+	"github.com/go-gem/sessions"
 )
 
 func TestVersion(t *testing.T) {
@@ -17,5 +20,23 @@ func TestVersion(t *testing.T) {
 func TestName(t *testing.T) {
 	if name != Name() {
 		t.Errorf("Expected name: %q, got %q.\n", name, Name())
+	}
+}
+
+func TestServer_SetLogger(t *testing.T) {
+	var logger log.Logger
+	s := New()
+	s.SetLogger(logger)
+	if s.logger != logger {
+		t.Errorf("Failed to set logger")
+	}
+}
+
+func TestServer_SetSessionsStoret(t *testing.T) {
+	var store sessions.Store
+	s := New()
+	s.SetSessionsStore(store)
+	if s.sessionsStore != store {
+		t.Errorf("Failed to set sessions store")
 	}
 }
