51f30549134dbf027550e5ede6c92ac66ab79c50
HeadwindFly <HeadwindFly@163.com>
2016-11-16 16:28:32 +0800

    Added Request Body Limit Middleware

--- middleware/body_limit.go
+++ middleware/body_limit.go
@@ -1,1 +1,49 @@
+// Copyright 2016 The Gem Authors. All rights reserved.
+// Use of this source code is governed by a MIT license
+// that can be found in the LICENSE file.
+
 package middleware
+
+import (
+	"github.com/go-gem/gem"
+	"github.com/valyala/fasthttp"
+)
+
+// BodyLimit request body limit middleware.
+type BodyLimit struct {
+	Skipper Skipper
+
+	// Maximum allowed size for a request body,
+	// it's unit is byte.
+	Limit int
+}
+
+// NewBodyLimit returns BodyLimit instance by the
+// given limit.
+func NewBodyLimit(limit int) *BodyLimit {
+	return &BodyLimit{
+		Limit: limit,
+	}
+}
+
+// Handle implements Middleware's Handle function.
+func (bl *BodyLimit) Handle(next gem.Handler) gem.Handler {
+	if bl.Skipper == nil {
+		bl.Skipper = defaultSkipper
+	}
+
+	return gem.HandlerFunc(func(c *gem.Context) {
+		if bl.Skipper(c) {
+			next.Handle(c)
+			return
+		}
+
+		if c.Request.Header.ContentLength() > bl.Limit || c.Request.Header.ContentLength() > bl.Limit {
+			c.RequestCtx.SetStatusCode(fasthttp.StatusRequestEntityTooLarge)
+			c.RequestCtx.SetBodyString(fasthttp.StatusMessage(fasthttp.StatusRequestEntityTooLarge))
+			return
+		}
+
+		next.Handle(c)
+	})
+}
--- middleware/body_limit_test.go
+++ middleware/body_limit_test.go
@@ -6,28 +6,23 @@ package middleware
 
 import (
 	"bufio"
-	"bytes"
-	"net"
 	"testing"
 	"time"
 
 	"github.com/go-gem/gem"
+	"github.com/go-gem/gem/bytes"
 	"github.com/valyala/fasthttp"
 )
 
-func newContext() *gem.Context {
-	return &gem.Context{
-		RequestCtx: &fasthttp.RequestCtx{},
-	}
-}
+func TestBodyLimit(t *testing.T) {
+	bl := NewBodyLimit(7 * bytes.B)
 
-func TestGzip(t *testing.T) {
 	s := gem.New()
 
 	router := gem.NewRouter()
-	router.Use(NewGzip(CompressBestCompression))
-	router.GET("/", func(c *gem.Context) {
-		c.HTML(fasthttp.StatusOK, "Compress")
+	router.Use(bl)
+	router.POST("/", func(c *gem.Context) {
+		c.HTML(fasthttp.StatusOK, "OK")
 	})
 
 	s.Init(router.Handler)
@@ -37,7 +32,14 @@ func TestGzip(t *testing.T) {
 	var resp fasthttp.Response
 	ch := make(chan error)
 
-	rw.r.WriteString("GET / HTTP/1.1\r\n\r\n")
+	newLine := "\r\n"
+	reqStr := "POST / HTTP/1.1" + newLine +
+		"Content-Length:8" + newLine +
+		"Content-Type:application/x-www-form-urlencoded" + newLine +
+		newLine +
+		"name=123"
+
+	rw.r.WriteString(reqStr)
 	go func() {
 		ch <- s.Server.ServeConn(rw)
 	}()
@@ -52,12 +54,21 @@ func TestGzip(t *testing.T) {
 	if err := resp.Read(br); err != nil {
 		t.Fatalf("Unexpected error when reading response: %s", err)
 	}
-	if len(resp.Header.Peek("Content-Encoding")) > 0 {
-		t.Errorf("Expected no compress response, got compressed response")
+	if resp.StatusCode() != fasthttp.StatusRequestEntityTooLarge {
+		t.Errorf("Expected status code %d, got %d", fasthttp.StatusRequestEntityTooLarge, resp.StatusCode())
 	}
 
-	// Accept-Encoding: gzip
-	rw.r.WriteString("GET / HTTP/1.1\r\nAccept-Encoding: gzip\r\n\r\n")
+	router = gem.NewRouter()
+	router.Use(bl)
+	router.POST("/", func(c *gem.Context) {
+		c.HTML(fasthttp.StatusOK, "OK")
+	})
+
+	s.Init(router.Handler)
+
+	// Increase limit size to 8B.
+	bl.Limit = 8 * bytes.B
+	rw.r.WriteString(reqStr)
 	go func() {
 		ch <- s.Server.ServeConn(rw)
 	}()
@@ -72,12 +83,16 @@ func TestGzip(t *testing.T) {
 	if err := resp.Read(br); err != nil {
 		t.Fatalf("Unexpected error when reading response: %s", err)
 	}
-	if ce := resp.Header.Peek("Content-Encoding"); len(ce) == 0 || !bytes.Equal(ce, []byte("gzip")) {
-		t.Errorf("Expected Content-Encoding %q, got %q", "gzip", ce)
+	if resp.StatusCode() != fasthttp.StatusOK {
+		t.Errorf("Expected status code %d, got %d", fasthttp.StatusOK, resp.StatusCode())
 	}
 
-	// Accept-Encoding: deflate, gzip
-	rw.r.WriteString("GET / HTTP/1.1\r\nAccept-Encoding: deflate, gzip\r\n\r\n")
+	// Skip body limit middleware
+	bl.Skipper = func(c *gem.Context) bool {
+		return true
+	}
+
+	rw.r.WriteString(reqStr)
 	go func() {
 		ch <- s.Server.ServeConn(rw)
 	}()
@@ -92,65 +107,7 @@ func TestGzip(t *testing.T) {
 	if err := resp.Read(br); err != nil {
 		t.Fatalf("Unexpected error when reading response: %s", err)
 	}
-	if ce := resp.Header.Peek("Content-Encoding"); len(ce) == 0 || !bytes.Equal(ce, []byte("gzip")) {
-		t.Errorf("Expected Content-Encoding %q, got %q", "gzip", ce)
+	if resp.StatusCode() != fasthttp.StatusOK {
+		t.Errorf("Expected status code %d, got %d", fasthttp.StatusOK, resp.StatusCode())
 	}
-
-	// Accept-Encoding: deflate
-	rw.r.WriteString("GET / HTTP/1.1\r\nAccept-Encoding: deflate\r\n\r\n")
-	go func() {
-		ch <- s.Server.ServeConn(rw)
-	}()
-	select {
-	case err := <-ch:
-		if err != nil {
-			t.Fatalf("return error %s", err)
-		}
-	case <-time.After(100 * time.Millisecond):
-		t.Fatalf("timeout")
-	}
-	if err := resp.Read(br); err != nil {
-		t.Fatalf("Unexpected error when reading response: %s", err)
-	}
-	if ce := resp.Header.Peek("Content-Encoding"); len(ce) == 0 || !bytes.Equal(ce, []byte("deflate")) {
-		t.Errorf("Expected Content-Encoding %q, got %q", "deflate", ce)
-	}
-}
-
-type readWriter struct {
-	net.Conn
-	r bytes.Buffer
-	w bytes.Buffer
-}
-
-var zeroTCPAddr = &net.TCPAddr{
-	IP: net.IPv4zero,
-}
-
-func (rw *readWriter) Close() error {
-	return nil
-}
-
-func (rw *readWriter) Read(b []byte) (int, error) {
-	return rw.r.Read(b)
-}
-
-func (rw *readWriter) Write(b []byte) (int, error) {
-	return rw.w.Write(b)
-}
-
-func (rw *readWriter) RemoteAddr() net.Addr {
-	return zeroTCPAddr
-}
-
-func (rw *readWriter) LocalAddr() net.Addr {
-	return zeroTCPAddr
-}
-
-func (rw *readWriter) SetReadDeadline(t time.Time) error {
-	return nil
-}
-
-func (rw *readWriter) SetWriteDeadline(t time.Time) error {
-	return nil
 }
