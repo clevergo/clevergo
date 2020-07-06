ec585dec4e7fdb215735465c73b129eb669cf84a
razonyang <razonyang@gmail.com>
2020-07-06 23:00:41 +0800

    Change LICENSE from BSD to MIT

--- clevergo.go
+++ clevergo.go
@@ -1,5 +1,5 @@
 // Copyright 2020 CleverGo. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be found
+// Use of this source code is governed by a MIT license that can be found
 // in the LICENSE file.
 
 // Package clevergo is a trie based high performance HTTP request router.
